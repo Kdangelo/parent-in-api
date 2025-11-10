22fcd2646905cc5a87ad6bd60dfc0b9dbaa82f93
Enrique Castillo <enriquecastillosoy@gmail.com>
2025-11-10 12:54:17 -0500

    DTO actualizado directo en la rama develop por ser cambio pequeño @Matches

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString, IsOptional, IsBoolean, MinLength, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsString, IsOptional, IsBoolean, MinLength, IsNotEmpty, Matches } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class CreateUserDto {
@@ -10,8 +10,9 @@ export class CreateUserDto {
   @IsNotEmpty({ message: 'La contraseña es requerida' })
   @IsString({ message: 'La contraseña debe ser un texto' })
   @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%?&])[A-Za-z\d@$!%?&]/,
+  { message: 'La contraseña debe contener mayúsculas, minúsculas, números y caracteres especiales' })
   password: string;
-
   @IsOptional()
   @IsString({ message: 'El nombre debe ser un texto' })
   @IsNotEmpty({ message: 'El nombre no puede estar vacío si se proporciona' })
