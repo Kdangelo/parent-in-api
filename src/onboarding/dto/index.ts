f387b0acdf7766432a05d16623bfb7699f6f22e6
Enrique Castillo <168988661+kikecastillocolombia@users.noreply.github.com>
2026-01-23 10:01:36 -0500

    Remove export of PostLicenciaDto

--- src/onboarding/dto/index.ts
+++ src/onboarding/dto/index.ts
@@ -2,4 +2,4 @@ export { UserDataDto } from './user-data.dto';
 export { ParentalUserDto } from './parental-user.dto';
 export { PreLicenciaDto } from './pre-licencia.dto';
 export { LicenciaDto } from './licencia.dto';
-export { PostLicenciaDto } from './post-licencia.dto';
+
